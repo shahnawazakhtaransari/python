5b58c90014860a168532ad6825e44b30211c5375
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-09 12:34:09 +0000

    1

---
+++ basics/oddeven_positivenegative.py
@@ -0,0 +1,12 @@
+x=int(input("enter a number"))
+# if x%2==0:
+#     print("It is an even number which is ",x)
+# else:
+#     print("odd number")    
+#=============================
+if x>0:
+    print("positive number!")
+elif x<0:
+    print("negative number!")    
+else:
+    print("number is 0")    
