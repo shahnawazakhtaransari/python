39a48906399b413b59cdab1c5609d979fb183e26
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-12 21:34:36 +0000

    factorial

--- basics/factorial.py
+++ basics/factorial.py
@@ -3,3 +3,11 @@ f=1
 for i  in  range(1,x+1):
     f=f*i
 print(f)
+# def factorial(num):
+#     f=1
+#     for i in range(1,num+1):
+#         f=f*i
+#     print(f) 
+# #source code
+# num=int(input("enter a number!"))
+# factorial(num)
