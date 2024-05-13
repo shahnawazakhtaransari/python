e4b77d9cfdf6c803b88c64434503bc5ccbfa3ec8
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-13 10:55:07 +0000

    perfect

--- basics/perfectSquare.py
+++ basics/perfectSquare.py
@@ -4,4 +4,18 @@ sahi=sqrt(n)
 if sahi==int(sahi):
     print("perfect")
 else:
-    print("Not so Perfect")    
+    print("Not so Perfect")  
+      
+# from math import sqrt
+# def isPerfectSquare(x):
+#     if x >= 0:
+#         sr = int(sqrt(x))
+#         return (sr * sr) == x
+#     return False
+
+
+# n = 84
+# if isPerfectSquare(n):
+#     print("True")
+# else:
+#     print("False")
