47e6984f469d797829a84545c979b005095a9d89
Shahnawaz Akhtar Ansari <73360721+shahnawazakhtaransari@users.noreply.github.com>
2024-05-21 05:07:32 +0000

    duplicates

--- basics-2/duplicate in list.py
+++ basics-2/duplicate in list.py
@@ -1,8 +1,20 @@
-# Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
-b=input("Enter the elements of the list being separated by spaces:")
-list=b.split()
+# # Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other.
+# b=input("Enter the elements of the list being separated by spaces:")
+# list=b.split()
+# print(list)
+# if len(b) == len(set(b)):#Sets do not allow duplicate values therefore if len is equal it means no duplicate values
+#     print("no duplicate values")
+# else:
+#     print("Duplicate values are present")        
+#different approach
+b = input("enter the elements of the list being separated by spaces: ")
+list = b.split()
 print(list)
-if len(b) == len(set(b)):#Sets do not allow duplicate values therefore if len is equal it means no duplicate values
-    print("no duplicate values")
-else:
-    print("Duplicate values are present")        
+dic = {}
+
+for i in list:
+    if i in dic:
+        print('error')
+        break
+    else: 
+        dic[i] = 1
